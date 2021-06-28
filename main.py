9b0b42010a3b9d689237b17849bbd3d39f564531
mongoz <makarivanov17y@gmail.com>
2021-06-28 21:04:24 +0300

    multi-user

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import telebot
 
 API_TOKEN = '1826894325:AAFbdkJPrUSfQ9rP5oap20tASUXXvZsNczs'
 shop_list_bot = telebot.TeleBot(API_TOKEN)
-last_message_id = None
 
-# telebot.types.ReplyKeyboardMarkup   чтобы появились кнопки, чтобы была логика
+# telebot.types.ReplyKeyboardMarkup   чтобы появились кнопки для юзера
 # telebot.types.ReplyKeyboardMarkup
 
 command = ["Добавить в список", "Показать список", "Удалить из списка"]
 key_boards = telebot.types.ReplyKeyboardMarkup()
 key_boards.row(*command)
 
+# chat_id = {shop_list, current_op, last_message_id}
 shop_list = []
-current_op = None  # Текущая операция 'добавить'.'показать'
+# current_op = None  # Текущая операция 'добавить'.'показать'
+# last_message_id = None
+
+# словарь нужен, если появляется новый юзер то мы будем обновлять данные
+user_data = {}
 
 
 # Логика
-# 1. Прописываем команду старт
+# 1. Прописываем команду старт. она ответит сообщением
 @shop_list_bot.message_handler(commands=['start'])
 def start(message):
     # передаем reply_markup, как параметр. не просто приветсвие, но и поле для навигации
@@ -26,46 +30,63 @@ def start(message):
 # Команды бота для списка(добавить. показать. удалить)
 @shop_list_bot.message_handler(content_types=['text'])
 def commands(message):
-    global current_op  # Почему глобал потому что будем менять в процессе написания
-    global shop_list  # Почему глобал потому что будем менять в процессе написания
-    global last_message_id
+    # global current_op  # Почему глобал потому что будем менять в процессе написания
+    # global shop_list  # Почему глобал потому что будем менять в процессе написания
+    # global last_message_id
+    print(user_data)
+    if message.chat.id not in user_data:
+        user_data[message.chat.id] = [[], None, None]  # значением будет список
     if message.text == "Добавить в список":
-        current_op = 'добавить'
+        user_data[message.chat.id][1] = 'добавить'
+        # current_op = 'добавить'
         shop_list_bot.reply_to(message, "Теперь добавьте продукты в список")
     elif message.text == 'Показать список':
-        current_op = 'показать'                              # если лист пустой то вернет ' ' пустую строку
-        shop_list_bot.reply_to(message, ", ".join(shop_list)if shop_list else '')  # сообщение кленту, когда он
+        user_data[message.chat.id][1] = 'показать'
+        # current_op = 'показать'  # если лист пустой то вернет ' ' пустую строку
+        shop_list_bot.reply_to(message, ", ".join(user_data[message.chat.id][0]) if user_data[message.chat.id][0] else 'empty')
+        # сообщение кленту, когда он
         # запросил показать что в списке. метод .join достает элементы из списка и записывает их через запятую
+        # если что-то есть нулевым элементом мы это покажем если нет, то не покажем(или покажем сообщение )
     elif message.text == "Удалить из списка":
-        current_op = 'удалить'
+        user_data[message.chat.id][1] = 'удалить'
+        # current_op = 'удалить'
         key_boards_rm = telebot.types.InlineKeyboardMarkup()  # Кнопка удалить
-        for elem in shop_list:
+        for elem in user_data[message.chat.id][0]:
             key_boards_rm.add(telebot.types.InlineKeyboardButton(elem, callback_data=elem))  # когда мы клацаем на
             # элемент  он выделяется
-        response = shop_list_bot.reply_to(message, "Теперь вы можете удалить елементы", reply_markup=key_boards_rm)
-        last_message_id = response.message_id  # чтобы потом потом можно было удалить
+        response = shop_list_bot.reply_to(message, "Теперь вы можете удалить элементы", reply_markup=key_boards_rm)
+        user_data[message.chat.id][2] = response.message_id  # чтобы потом потом можно было удалить
+        # информация персонализирована под человека
     else:
-        if current_op == "добавить":
-            shop_list.append(message.text)
+        if user_data[message.chat.id][1] == "добавить":
+            user_data[message.chat.id][0].append(message.text)
             shop_list_bot.reply_to(message, f"успешно добавлено {message.text}")
 
 
 @shop_list_bot.callback_query_handler(func=lambda x: True)  # лямбдой говорим что будем всегда заходить в этот декоратор
 def callback_handler(call):  # а зайду я когда нажму на элемент. потому что есть режим сallback_data
-    global shop_list
+    # global shop_list
     index_remove = None
-    for i in range(len(shop_list)):  # удаляем по индексу
-        if shop_list[i] == call.data:
+    for i in range(len(user_data[call.message.chat.id][0])):  # удаляем по индексу
+        if user_data[call.message.chat.id][0][i] == call.data:
             index_remove = i
             break
     if index_remove is not None:
-        del shop_list[index_remove]
+        del user_data[call.message.chat.id][0][index_remove]
 
     key_boards_rm = telebot.types.InlineKeyboardMarkup()  # Кнопка удалить
     for elem in shop_list:
         key_boards_rm.add(telebot.types.InlineKeyboardButton(elem, callback_data=elem))
         # Чтобы обновить структуру(markup), где указаны элементы нужно знать id-cообщения(удалить из листа).
-    shop_list_bot.edit_message_reply_markup(call.message.chat.id, last_message_id, reply_markup=key_boards_rm)
+    shop_list_bot.edit_message_reply_markup(call.message.chat.id, user_data[call.message.chat.id][2], reply_markup=key_boards_rm)
 
 
 shop_list_bot.polling()
+
+
+# после пробы работы для мульти-юзера  не добавляет в список
+# ошибка была , что поставил неправильный индекс словаря в 59 строке
+# проблема не удаляет из списка. просто пропадает
+# в 79 строке пропустил мусорную переменную из-за этого условие не срабатывало
+# user_data это словарь и он изменяемый. поэтому если там есть ключик мы просто
+# добавил ссылку ввиде 'add'
